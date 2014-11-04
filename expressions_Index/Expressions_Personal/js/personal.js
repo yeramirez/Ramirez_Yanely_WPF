e6d275d7c395dcdadccc5b13eac2b3f9e6f90c85
yeramirez <yxnely@Yanelys-MacBook-Pro.local>
2014-11-03 21:50:32 -0500

    Insert First Prompt
    Prompted how many fill ups the user has

--- expressions_Index/Expressions_Personal/js/personal.js
+++ expressions_Index/Expressions_Personal/js/personal.js
@@ -8,5 +8,11 @@ Date
 
 //alert("Testing 1,2,3!");
 
- 
+ //How much money will I spend on gas in one month?
+ //How much money did you spend on the first fill-up.
 
+ //Ask user how many fill ups they do in a month?
+ var gasFillUps = prompt("How many fill ups do you get in a month?");
+ console.log(gasFillUps);
+
+ 
