03c8a83cedc1e02cca7a53157a76d4635ddaf092
yeramirez <yxnely@Yanelys-MacBook-Pro.local>
2014-11-03 21:53:43 -0500

    Prompt user again
    Ask user how many gallons it takes to fill up the car

--- expressions_Index/Expressions_Personal/js/personal.js
+++ expressions_Index/Expressions_Personal/js/personal.js
@@ -15,4 +15,15 @@ Date
  var gasFillUps = prompt("How many fill ups do you get in a month?");
  console.log(gasFillUps);
 
+ //Ask user how much gas costs per gallon
+ var gasPrice = prompt("How much does gas cost at your gas station?");
+ console.log(gasPrice);
+
+ //Ask user how many gallons it takes to fill up their tank?
+ var tankGallons = prompt("How many gallons does it take your car to fill up?");
+ console.log(tankGallons);
+
  
+
+
+
