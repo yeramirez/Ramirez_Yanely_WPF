95ba45bcdc8e53be90eb7daac2d46fb72fbdcd93
yeramirez <yxnely@Yanelys-MacBook-Pro.local>
2014-11-03 22:50:40 -0500

    Asking More Questions
    User gets asked how much they get paid an hour

---
+++ expressions_Index/Expressions_Industry/js/industry.js
@@ -0,0 +1,20 @@
+
+ /*
+Yanely Ramirez
+Title
+WPF 1411
+Date
+ */
+
+//alert("Testing 1,2,3!");
+
+//What kind of question will you be figuring out from the industry aspect?
+ //How much money will you earn after 3 months of blood, sweat, and tears based on weeks?
+
+ //Ask how many hours will you work?
+ var hours = prompt("How many hours will I have worked this week?");
+ var months = 3
+ var hours = parseInt(hours) * months
+ console.log("I will work "+ hours + " hours in "+months+" months.");
+
+ //Ask how much you will get paid by the hour
--- expressions_Index/Expressions_Personal/js/personal.js
+++ expressions_Index/Expressions_Personal/js/personal.js
@@ -1,9 +1,9 @@
 
  /*
 Yanely Ramirez
-Title
+Personal Expressions
 WPF 1411
-Date
+11-02-2014
  */
 
 //alert("Testing 1,2,3!");
@@ -12,19 +12,19 @@ Date
  //How much money did you spend on the first fill-up.
 
  //Ask user how many fill ups they do in a month?
- var gasFillUps = prompt("How many fill ups do you get in a month?");
- console.log(gasFillUps);
+ var gasFillUps = prompt("We are going to calculate how much money you spend on gas each month! How many fill ups do you get in a month?");
+ console.log("You get " +gasFillUps+ " fill ups each month.");
 
  //Ask user how much gas costs per gallon
  var gasPrice = prompt("How much does gas cost at your gas station?");
- console.log(gasPrice);
+ console.log("The gas you buy costs $"+gasPrice+" per gallon.");
 
  //Ask user how many gallons it takes to fill up their tank?
  var tankGallons = prompt("How many gallons does it take your car to fill up?");
- console.log(tankGallons);
+ console.log("Your car uses "+tankGallons+" gallons of gas.");
 
  //Multiply the needed numbers
- var results = parseInt(gasFillUps) * parseInt(gasPrice) * parseInt(tankGallons);
+ var results = parseInt(gasFillUps) * Number(gasPrice) * parseInt(tankGallons);
  console.log("You spend $"+ results + " on gas each month.");
 
 
