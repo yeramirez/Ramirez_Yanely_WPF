e721d2c5646fcdef6c262775daa640e45304d72d
yeramirez <yxnely@Yanelys-MacBook-Pro.local>
2014-11-03 23:00:12 -0500

    Final Calculation
    Calculate how much money will be made with the numbers inserted before.

--- expressions_Index/Expressions_Industry/js/industry.js
+++ expressions_Index/Expressions_Industry/js/industry.js
@@ -9,15 +9,24 @@ Date
 //alert("Testing 1,2,3!");
 
 //What kind of question will you be figuring out from the industry aspect?
- //How much money will you earn after 3 months of blood, sweat, and tears based on weeks?
+ //How much money will you earn after months of blood, sweat, and tears based on weeks?
 
  //Ask how many hours will you work?
  var hours = prompt("How many hours will I have worked this week?");
- console.log("I will work "+ hours + " hours in a week.");
+ console.log("You will work "+ hours + " hours in a week.");
 
  //Ask how much you will get paid by the hour
  var money = prompt("How much will you get paid an hour?");
  console.log("You will get paid $"+ money + " an hour.");
 
  //Ask how many months you will be working
+ var months = prompt("How many months will you be working?");
+ console.log("You are going to be working "+months+" months.");
+
+ //Calculate how much money will be earned at the end.
+ var totalMoney = parseInt(hours) * Number(money) * parseInt(months);
+ console.log("You will get paid $" + totalMoney + " at the end of "+months+" months.");
+
+//Finished
+
  
