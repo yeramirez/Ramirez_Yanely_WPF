eee9dc2a468dc2de42cdf2888fcb22e84e48efc2
yeramirez <yxnely@Yanelys-MacBook-Pro.local>
2014-11-05 17:17:03 -0500

    Adding consoles to all
    Consoling everything!

--- expressions_Index/Expressions_Wacky/js/wacky.js
+++ expressions_Index/Expressions_Wacky/js/wacky.js
@@ -11,7 +11,7 @@ Date
  //How many monkeys are jumping on the bed?
 
  var userMonkey = prompt("How many monkeys are jumping on the bed (1-5)?");
- var monkeysonBed = ["No more monkeys",1,2,3,4,5];
+ var monkeysonBed = ["No more monkeys jumping on the bed!",1,2,3,4,5];
  alert("One fell off and bumped his head.");
 
  //Set up first monkey jumping on the bed.
@@ -21,20 +21,20 @@ Date
 
  userMonkey--;
  alert(monkeysonBed[userMonkey] + " jumping on the bed");
-
-
- userMonkey--;
- alert(monkeysonBed[userMonkey] + " jumping on the bed");
-
+ console.log(monkeysonBed[userMonkey]+ " left on bed");
 
  userMonkey--;
  alert(monkeysonBed[userMonkey] + " jumping on the bed");
-
-
- userMonkey--;
- alert(monkeysonBed[userMonkey] + " jumping on the bed");
-
+ console.log(monkeysonBed[userMonkey]+ " left on bed");
 
  userMonkey--;
  alert(monkeysonBed[userMonkey] + " jumping on the bed");
+ console.log(monkeysonBed[userMonkey]+ " left on bed");
 
+ userMonkey--;
+ alert(monkeysonBed[userMonkey] + " jumping on the bed");
+ console.log(monkeysonBed[userMonkey]+ " left on bed");
+
+ userMonkey--;
+ alert(monkeysonBed[userMonkey] + " jumping on the bed");
+ console.log(monkeysonBed[userMonkey]+ " left on bed");
