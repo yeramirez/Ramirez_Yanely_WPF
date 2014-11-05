a50a9227e43281f9c7959e2c82ae55ad45e2de2d
yeramirez <yxnely@Yanelys-MacBook-Pro.local>
2014-11-05 17:11:42 -0500

    Wacky
    How many monkeys are jumping on the bed?

--- expressions_Index/Expressions_Wacky/js/wacky.js
+++ expressions_Index/Expressions_Wacky/js/wacky.js
@@ -8,5 +8,33 @@ Date
 
 //alert("Testing 1,2,3!");
 
+ //How many monkeys are jumping on the bed?
 
+ var userMonkey = prompt("How many monkeys are jumping on the bed (1-5)?");
+ var monkeysonBed = ["No more monkeys",1,2,3,4,5];
+ alert("One fell off and bumped his head.");
+
+ //Set up first monkey jumping on the bed.
+ userMonkey--;
+ alert(monkeysonBed[userMonkey] + " jumping on the bed");
+ console.log(monkeysonBed[userMonkey]+ " left on bed");
+
+ userMonkey--;
+ alert(monkeysonBed[userMonkey] + " jumping on the bed");
+
+
+ userMonkey--;
+ alert(monkeysonBed[userMonkey] + " jumping on the bed");
+
+
+ userMonkey--;
+ alert(monkeysonBed[userMonkey] + " jumping on the bed");
+
+
+ userMonkey--;
+ alert(monkeysonBed[userMonkey] + " jumping on the bed");
+
+
+ userMonkey--;
+ alert(monkeysonBed[userMonkey] + " jumping on the bed");
 
