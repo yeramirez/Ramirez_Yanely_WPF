fc99b28f03913c6ebc5fa6976f3e067b73932e75
yeramirez <yxnely@Yanelys-MacBook-Pro.local>
2014-11-06 00:40:34 -0500

    Finished Masterpiece
    All three have been done.

--- Conditionals_worksheet/js/tickets.js
+++ Conditionals_worksheet/js/tickets.js
@@ -13,12 +13,27 @@ WPF 1411
  //If you are 10- you get the discount
  //If you watch the movie between 3pm-5pm you get the discount
 
- var senior = 55;
+
+
  var child = 10;
- var timeEarly = 3;
- var timeLate = 5;
+ var senior = 55;
 
- //Ask user for age?
- var age = 25;
+ var age = prompt("What is your age? We are going to determine if you are eligible for a discount!");
+ if(age<=child || age>=senior){
+  console.log("You are able to receive the discount and will only have to pay $7.00.")
 
- //The user must have at least one so you use the or (||) symbol
+ } else {
+  console.log("I'm sorry, you cannot receive the discount.")
+ }
+
+ var showTime = prompt("What time will you watch the show?");
+ if(showTime>2 || showTime<6){
+  console.log("You are able to receive the discount and will only have to pay $7.00.");
+
+ } else{
+  console.log("I'm sorry, you cannot receive the discount.");
+
+ }
+
+ console.log("If you are able to recieve the discount at least once, then you are able to receive the discount. If you are able to\n" +
+ "receive the discount 'twice' then you are only able to use it once.");
