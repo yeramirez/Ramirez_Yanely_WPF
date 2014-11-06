e7dbfc37fd98590d6cb9d83d600d2de396e79445
yeramirez <yxnely@Yanelys-MacBook-Pro.local>
2014-11-05 22:53:37 -0500

    The math has been done!
    I got all the math to match up!

--- Conditionals_worksheet/js/conditionals.js
+++ Conditionals_worksheet/js/conditionals.js
@@ -13,12 +13,25 @@ WPF 1411
  //How much efficiency does your car get (mpg)?
  var gasEfficiency = 32;
 
- //How much do you have in your tank (%)?
- var gasInTank = 50;
-
  //How many gallons can your car hold (g)?
- var gasIntake = 10;
+ var gasCapacity = 10;
 
- 
+ //How much do you have in your tank (%)?
+ var gasInTank = .75;
+ var gasInTankGallons = gasInTank * gasCapacity;
+
+
+ //Make an equation to find out how many miles you can travel until you are empty.
+ var miles = gasInTankGallons * gasEfficiency;
+
+ //If statement
+ if(miles>=200) {
+  console.log("Yes, you can make it without stopping! You go Glen Coco!")
+
+ } else {
+  console.log("You only have " + gasInTankGallons + " gallons left in your tank! You better get gas now while you can!")
+ }
+
+
 
 
