1f7c21be70683e39b7c7dee6e80771edc919db15
yeramirez <yxnely@Yanelys-MacBook-Pro.local>
2014-11-05 23:38:58 -0500

    Assigned Numbers
    Assigned numbers to the age and time restrictions of the discounts

--- Conditionals_worksheet/js/tickets.js
+++ Conditionals_worksheet/js/tickets.js
@@ -8,4 +8,14 @@ WPF 1411
 
 //alert("Testing 1,2,3!");
 
+ //Determining if you can get the discount of $7
+ //If you are a senior of 55+ you get the discount
+ //If you are 10- you get the discount
+ //If you watch the movie between 3pm-5pm you get the discount
+
+ var senior = 55;
+ var child = 10;
+ var timeEarly = 3;
+ var timeLate = 5;
+
  
