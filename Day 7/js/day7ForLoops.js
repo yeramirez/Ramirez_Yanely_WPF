0cc66247ee6241e041043717cf1b2764cb25185b
yeramirez <yxnely@Yanelys-MacBook-Pro.local>
2014-11-07 20:24:31 -0500

    Finished While Loops
    Yay

---
+++ Day 7/js/day7ForLoops.js
@@ -0,0 +1,10 @@
+
+ /*
+Yanely Ramirez
+For Loops
+WPF 1411
+Date
+ */
+
+alert("Testing 1,2,3!");
+
--- Day 7/js/day7WhileLoop.js
+++ Day 7/js/day7WhileLoop.js
@@ -8,5 +8,34 @@ WPF 1411
 
 //alert("Testing 1,2,3!");
 
- 
+ //Basic While Loops
+ //The while loop will loop through a block of code as long as the condition remains true
+
+ var counter = 0; //Step one initialize the counting variable
+
+ while(counter<20){ //condition - will run as long as this is true
+  console.log("Somethings will never end "+counter);
+
+  //Update or change the counter variable
+
+  counter++;
+
+ }
+ console.log("End of loop - Last counter variable "+counter);
+
+ //Do While Loop
+ //it's sort of like the while loop
+ //Difference is, the code will run at LEAST once!
+
+ var i = 0; //counter variable
+
+ do{
+  console.log("The counting number is " + i);
+
+  //Update or change the counting the i
+
+  i+=5;
+
+
+ }while(i<50);
 
