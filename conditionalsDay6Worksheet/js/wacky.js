5eb76522b19ec2f67ceade2697a651264f03f7cf
yeramirez <yxnely@Yanelys-MacBook-Pro.local>
2014-11-07 23:49:24 -0500

    Adding comments
    Adding comments to the conditionals.

--- conditionalsDay6Worksheet/js/wacky.js
+++ conditionalsDay6Worksheet/js/wacky.js
@@ -12,16 +12,16 @@ WPF 1411
 
  var age = prompt("How old are you?");
 
- if(age>=10){
+ if(age>=10){ // We are testing how old the player is, if they are older than 10 then they can play.
   console.log("Okay, let's play a game!");
 
- } else if (age<10){
+ } else if (age<10){ // If the player is younger than 10 then they are not able to play.
   console.log("Sorry, you are not old enough to play this game! :(");
 
- } else {
+ } else { // If the player enters something silly then they are prompted to enter a real number
   console.log("Please enter a real age.");
 
  }
 
- //
+
 
