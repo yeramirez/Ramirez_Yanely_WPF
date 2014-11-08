4d2eab25ccdee83af29fee5f0c404076a001a0be
yeramirez <yxnely@Yanelys-MacBook-Pro.local>
2014-11-07 23:47:16 -0500

    Entering If's and Else If's
    Making conditionals

--- conditionalsDay6Worksheet/js/wacky.js
+++ conditionalsDay6Worksheet/js/wacky.js
@@ -6,5 +6,22 @@ WPF 1411
 11-07-2014
  */
 
-alert("Testing 1,2,3!");
+//alert("Testing 1,2,3!");
+
+ //Let's play a game!
+
+ var age = prompt("How old are you?");
+
+ if(age>=10){
+  console.log("Okay, let's play a game!");
+
+ } else if (age<10){
+  console.log("Sorry, you are not old enough to play this game! :(");
+
+ } else {
+  console.log("Please enter a real age.");
+
+ }
+
+ //
 
