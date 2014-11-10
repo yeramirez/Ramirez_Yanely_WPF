08f43870b0493881b2dd57d6fe6d021daa5e76ce
yeramirez <yxnely@yanelys-mbp.gateway.pace.com>
2014-11-09 20:40:20 -0500

    Fixed the troubleshooting!
    Found out how to break the loops!

--- conditionalsDay6Worksheet/js/wacky.js
+++ conditionalsDay6Worksheet/js/wacky.js
@@ -15,6 +15,47 @@ WPF 1411
  if(age>=10){ // We are testing how old the player is, if they are older than 10 then they can play.
   console.log("Okay, let's play a game!");
 
+
+     //Let's make this into a game that checks how smart we are.
+     //So let's ask some simple questions.
+
+     alert("We are going to check what your age is depending on how you answer these simple questions! :)");
+
+     //Ask question one.
+     var quest1 = prompt("What is the sum of 2+2?");
+
+     if(quest1 == 4){ // We are testing to see if the user inserted the correct answer.
+         console.log("Okay! Next question!");
+
+         //Ask a second question! This time, harder (twss).
+         var quest2 = prompt("What is 12 * 12?");
+
+         if(quest2 == 144){ // We are testing to see if the user inserted the correct answer.
+             console.log("Okay! Next question!");
+
+             //Ask a third question (level 3)
+             var quest3 = prompt("What is (3 * 3) + 24");
+
+             if(quest3 == 31){
+                 console.log("Okay! Next question!");
+
+             } else {
+                 console.log()
+             }
+
+         } else { // Say this if the question was answered incorrectly.
+             console.log("I'm sorry you are incorrect! Your brain age is: 10 years old.");
+
+
+
+         }
+
+     } else { // Say this if the question was answered incorrectly.
+         console.log("I'm sorry you are incorrect! Your brain age is: 2 years old.");
+     }
+
+
+
  } else if (age<10){ // If the player is younger than 10 then they are not able to play.
   console.log("Sorry, you are not old enough to play this game! :(");
 
@@ -23,34 +64,6 @@ WPF 1411
 
  }
 
- //Let's make this into a game that checks how smart we are.
- //So let's ask some simple questions.
-
- alert("We are going to check what your age is depending on how you answer these simple questions! :)");
-
- //Ask question one.
- var quest1 = prompt("What is the sum of 2+2?");
-
- if(quest1 == 4 || quest1 == "4"){ // We are testing to see if the user inserted the correct answer.
-  console.log("Okay! Next question!");
-
- } else { // Say this if the question was answered incorrectly.
-  console.log("I'm sorry you are incorrect! Your brain age is: 2 years old.");
-
- }
-
- //Ask a second question! This time, harder (twss).
- var quest2 = prompt("What is 12 * 12?");
-
- if(quest2 == 144 || quest2 == "144"){
-  console.log("Okay!, Next question!")
-
- } else {
-  console.log("I'm sorry you are incorrect! Your brain age is: 10 years old.");
-
- }
-
- //Ask a third question (level 3)
 
 
 
@@ -69,4 +82,8 @@ WPF 1411
 
 
 
- 
+
+
+
+
+
