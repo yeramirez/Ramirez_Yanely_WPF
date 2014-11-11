85207160e90f7367e802bf600e527917d1e802b8
yeramirez <yxnely@Yanelys-MacBook-Pro.local>
2014-11-10 23:01:29 -0500

    Working- For the most part
    This is up and running.

--- Loops/js/whileLoops.js
+++ Loops/js/whileLoops.js
@@ -11,9 +11,13 @@ WPF 1411
  //Let's start a loop!
  //Let's do the counting game!
 
- var game = prompt("Let's play a game!");
+ var game = alert("Let's play a game!"); //Let them know what we are doing!
 
- var counting = prompt("Pick any number!\nAnd let's start counting!");
+ var i = prompt("Pick any number!\nLet's start counting!");
 
- 
+ while(i<3) { //So while this is true, the counter will keep going.
+  console.log("This is forever " + i);
+
+ }
+
 
