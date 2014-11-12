e571a542882fa29f89f505e67bd51027218bb735
yeramirez <yxnely@yanelys-mbp.gateway.pace.com>
2014-11-12 16:05:47 -0500

    Do While loop!
    Creating a problem for the do while loop!

---
+++ Loops/js/doWhile.js
@@ -0,0 +1,17 @@
+
+ /*
+Yanely Ramirez
+Do While
+WPF 1411
+11-11-2014
+ */
+
+//alert("Testing 1,2,3!");
+
+ /*
+ I recently used the phrase "monkeys jumping on the bed", but was not able to correctly code it because
+ I did not know how to use loops quite yet, so here it is, correctly formatted...
+ */
+
+ //The user will be able to put in as many monkeys as they want on the bed.
+
