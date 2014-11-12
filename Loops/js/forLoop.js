d2f44a2f78f788d2b630d267b07e3f1c0c0c84a3
yeramirez <yxnely@yanelys-mbp.gateway.pace.com>
2014-11-12 13:08:36 -0500

    Problem solved for For Loops
    For Loops! I made a dumb problem, but really what could I do?

--- Loops/js/forLoop.js
+++ Loops/js/forLoop.js
@@ -5,10 +5,17 @@ WPF 1411
 11-11-2014
  */
 
-alert("Testing 1,2,3!");
+//alert("Testing 1,2,3!");
 
 //Suppose I had a group of 300 students (we're talking a big college here)
  //I want to separate them into groups of 20
  //I will get to see how many groups there are
 
- 
+ //Now we put in how we're going to separate them.
+
+ for(var i =300; i>0; i-=20){
+  console.log("Students equal " + i);
+
+ }
+
+ //This will now give a visual representation of how many groups there will be.
