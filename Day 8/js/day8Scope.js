0492d2ec0cb924ecfa37b9869dc8aa68b53d6046
yeramirez <yxnely@Yanelys-MacBook-Pro.local>
2014-11-12 19:22:38 -0500

    Arguments and Parameters
    Starting arguments and parameters

--- Day 8/js/day8Scope.js
+++ Day 8/js/day8Scope.js
@@ -8,3 +8,38 @@ WPF 1411
 
 //alert("Testing 1,2,3!");
 
+ //Variable Scope
+ //Variables inside and outside of a function
+
+ //We try not to use the same names for variables...BUT
+ //In large files, this is going to be impossible.
+
+ //When you create a variable inside of a function, it only exist
+ //inside of that one function.
+
+ //Create a variable for width  (main code)
+ var width =5; //Scoped outside of the function or scoped to main
+ console.log("Starting width in main code is " + width);
+
+ //Create a function that calculates the perimeter of a rectangle
+ function calcPeri(){
+
+  var width = 10; //This var is scoped to the function calcPeri
+  console.log("Inside of the function, the width is " + width + ".");
+  var height = 20;
+  var perimeter = width*2 + height*2;
+
+  console.log("Inside of function, the perimeter is " + perimeter +".")
+
+
+ }
+
+ console.log("Before function call " +width);
+ calcPeri();
+ console.log("After the function call "+width);
+
+ //What happens in vegas (function) stays in vegas (function).
+ //
+
+
+
