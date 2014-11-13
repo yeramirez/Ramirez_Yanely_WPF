b58988b78cd264adf346514b0deea324fd1d5f6a
yeramirez <yxnely@Yanelys-MacBook-Pro.local>
2014-11-12 22:11:31 -0500

    Fixed prompts
    Fixed the prompts to fit the adjusted primary question.

--- Loops_worksheet/js/validatingLoops.js
+++ Loops_worksheet/js/validatingLoops.js
@@ -10,15 +10,15 @@ WPF 1411
 
  //Make up a problem for the user to solve
 
- //Let's determine how many songs we can listen to in a certain amount of time
+ //Let's determine how much time it takes to listen to our playlist
 
 
  //We will first prompt the user for a amount of time (that they will listen to music)
 
- var time = prompt("How long will you be listening to music?");
+ var songs = prompt("How many songs are on your playlist?");
 
- //We will then prompt the user for the average time a song is
+ //We will then prompt the user for the average time a song in the playlist
 
  var songTime = prompt("On average, how long are the songs you are listening to?");
 
- 
+ //Now we will ask
