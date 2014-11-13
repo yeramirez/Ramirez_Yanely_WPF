479e40605eb9e86cf7b8a0812bae2d5ab60bb16f
yeramirez <yxnely@Yanelys-MacBook-Pro.local>
2014-11-12 23:04:34 -0500

    Figuring it out
    Inserted functions to make the code better.

--- Functions_worksheet/js/circumference.js
+++ Functions_worksheet/js/circumference.js
@@ -10,21 +10,38 @@ WPF 1411
 
  //Calculate the circumference of a circle
 
- var radius = prompt("Please assign a radius to your circle!");
+ //var radius = prompt("Please assign a radius to your circle!");
 
  //Let's validate to see if it fits
 
- while(radius ==="" || isNaN(radius)){
+ //while(radius ==="" || isNaN(radius)){
 
-  radius = prompt("Please only type in numbers and don't leave blank!\nEnter a radius in numerical value.");
+  //radius = prompt("Please only type in numbers and don't leave this space blank!\nEnter a radius in numerical value.");
 
- }
+ //}
 
- //Now lets calculate
-
- var circumference = Number(radius) * 2 * Math.PI
 
  //Let's check the result
+ //console.log("The circumference of your circle is: " + circumference + "!");
 
 
+ userPrompt = prompt("Please assign a radius to your circle.");
 
+ while(userPrompt ==="" || isNaN(userPrompt)){
+
+  userPrompt = prompt("Please only type in numbers and don't leave this space blank!\nEnter a radius in numerical value.");
+
+  }
+ 
+
+function circumference(userPrompt){
+ var circumference = Number(userPrompt) * 2 * Math.PI;
+ return circumference;
+
+}
+
+circumference(userPrompt);
+ results = circumference(userPrompt);
+
+
+ console.log("The circumference of your circle is: " + results + ".");
