b2dfd567c9e78e86b2180284c0f1174ed6fae5b1
yeramirez <yxnely@Yanelys-MacBook-Pro.local>
2014-11-12 22:19:41 -0500

    Finished validating and it works!
    This is great!

--- Loops_worksheet/js/validatingLoops.js
+++ Loops_worksheet/js/validatingLoops.js
@@ -17,12 +17,26 @@ WPF 1411
 
  var songs = prompt("How many songs are on your playlist?");
 
+ while(songs ==="" || isNaN(songs)){
+
+  songs = prompt("Please only type in numbers and don't leave blank!\nEnter a number.");
+
+ }
+
  //We will then prompt the user for the average time a song in the playlist
 
  var songTime = prompt("On average, how long are the songs you are listening to (minutes)?");
 
+ while(songTime ==="" || isNaN(songTime)){
+
+  songTime = prompt("Please only type in numbers and don't leave blank!\nEnter a number.");
+
+ }
+
  //Now we will do the math
 
  var totalTime = (songs * songTime) / 60;
+
+ console.log("Your playlist will take " + totalTime + " hours.");
 
  
