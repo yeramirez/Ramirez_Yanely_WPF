085652c8f0d3572ab0a7d72dd29bb642da1e81fa
yeramirez <yxnely@Yanelys-MacBook-Pro.local>
2014-11-14 22:43:40 -0500

    Pringles Can Measurements
    Let’s get the measurements of a pringles can

--- day9FunctionsWorksheet/js/Functions_Wacky.js
+++ day9FunctionsWorksheet/js/Functions_Wacky.js
@@ -10,5 +10,11 @@ WPF 1411
 
  //Make up a wacky problem!
 
+ //Let's calculate the volume of a Pringles can and see if our arm can fit through it.
+ //Let's calculate the volume of the can given the measurements.
+
+ var radius = 1.5;
+ var height = 10.5;
+
  
 
