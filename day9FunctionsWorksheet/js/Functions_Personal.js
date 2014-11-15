30a5b38560800efae7d3b0dae04f0140dc590bd4
yeramirez <yxnely@Yanelys-MacBook-Pro.local>
2014-11-14 21:35:04 -0500

    We have our first problem!
    Created the personal problem to solve.

--- day9FunctionsWorksheet/js/Functions_Personal.js
+++ day9FunctionsWorksheet/js/Functions_Personal.js
@@ -8,5 +8,11 @@ WPF 1411
 
 //alert("Testing 1,2,3!");
 
+ //DISCLAIMER: I hate soda and am only doing this for the purpose of this assignment and no more.
+
  //Let's start a personal problem
 
+ //Let's find the volume of a soda can
+ //This is a personal problem for me because I like to drink a lot
+
+ 
