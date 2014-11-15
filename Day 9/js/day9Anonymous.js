2be5a227ebdc4d0e1f8e0e6bb000f430b78f0bdd
yeramirez <yxnely@Yanelys-MacBook-Pro.local>
2014-11-14 19:52:45 -0500

    Finished Anonymous - Starting Multiple
    Combining multiple functions

--- Day 9/js/day9Anonymous.js
+++ Day 9/js/day9Anonymous.js
@@ -6,5 +6,68 @@ WPF 1411
 11-14-2014
  */
 
-alert("Testing 1,2,3!");
+//alert("Testing 1,2,3!");
+
+
+ /*
+ basic function
+ function functionName(){
+   //code goes here
+
+ }
+
+ Anon function
+ var functionName = function(){
+   //code goes here
+
+ };
+
+ */
+
+//A normal function to find the area of a triangle
+
+ function triangleArea(base,height){
+
+  var area = .5*base*height;
+  return area;
+
+
+ }
+
+ //function call - for normal function
+ var returnedArea = triangleArea(4,5);
+ console.log("Returned area for a normal function is "+returnedArea);
+
+
+//Anonymous function that calculates triangle area
+ var anonTriArea = function (base, height){
+    var area = .5*base*height;
+    return area;
+
+ };
+
+ //Function call for a non funtion
+ var returnedAnonArea = anonTriArea(2,3);
+ console.log("The returned area from the anon function is "+returnedAnonArea);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ Day 9/js/day9Multiple.js
@@ -0,0 +1,10 @@
+
+ /*
+Yanely Ramirez
+Combining Functions
+WPF 1411
+11-14-2014
+ */
+
+alert("Testing 1,2,3!");
+
